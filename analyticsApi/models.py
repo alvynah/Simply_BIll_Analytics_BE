31589c30238fda372e6a445008b4c904f25c58ec
alvynah <alvynahw@gmail.com>
2021-06-17 18:10:51 +0300

    Add rest framework

--- analyticsApi/models.py
+++ analyticsApi/models.py
@@ -7,12 +7,9 @@ class User(AbstractUser):
 	userId = models.AutoField(primary_key=True)
 	first_name=models.CharField(max_length=50)
 	last_name=models.CharField(max_length=50)
-	phone_number=models.IntegerField(unique=True)
+	phone_number=models.IntegerField(unique=True, null=True)
 	email=models.EmailField()
-	password=models.CharField(max_length=50)
+	password=models.CharField(max_length=255)
 	is_customer=models.BooleanField(default=False)
 	is_banker=models.BooleanField(default=False)
-	username=None
 
-	USERNAME_FIELD='phone_number'
-	REQUIRED_FIELDS=[]
