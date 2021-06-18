7069ad83901cc6b722c9f8f30e220c80b8b68a16
Alice-Githui <78473216+Alice-Githui@users.noreply.github.com>
2021-06-18 09:17:00 +0300

    Ft/admin (#3)
    * install django-rest-framework
    
    * create admin registration serializers, views and URLs
    
    * create activation model
    
    * create user login route and set JWT to get a session of a user
    
    * add userId to the sign up serializers fields

--- analyticsApi/serializers.py
+++ analyticsApi/serializers.py
@@ -1,12 +1,12 @@
 from rest_framework import serializers
-from .models import User
+from .models import User, Activation
 from django import forms
 
 
 class SignUpSerializer(serializers.ModelSerializer):
   class Meta:
     model = User
-    fields = ['username','first_name',  'last_name',  'phone_number', 'email','password']
+    fields = ['userId','username','first_name', 'last_name', 'phone_number', 'email','password']
     extra_kwargs = {
       "password": {'write_only': True}
     }
@@ -20,33 +20,30 @@ class SignUpSerializer(serializers.ModelSerializer):
           instance.save()   
           return instance
 
+# admin sign up serializer
+class AdminSignUpSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = User
+    fields = ['username','first_name',  'last_name',  'phone_number', 'email','password']
+    extra_kwargs = {
+      "password": {'write_only': True}
+    }
+
+  def create(self, validated_data):
+          password =validated_data.pop('password', None)
+          instance =self.Meta.model(**validated_data)
+          if password is not None:
+             instance.set_password(password)
+          instance.is_admin=True  
+          instance.save()   
+          return instance
+
+# activation serializers 
+class ActivationSerializer(serializers.ModelSerializer):
+  class Meta:
+    model=Activation
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-  # def save(self):
-  #   user = User(
-  #           username = self.validated_data['username'],
-  #           email = self.validated_data['email'],
-  #       )
-  #   password = self.validated_data['password']
-  #   confirm_password = self.validated_data['confirm_password']
-  #   if password != confirm_password:
-  #     raise serializers.ValidationError({'password': 'Passwords must match'})
-  #   user.set_password(password)
-  #   user.save()
-  #   return user
 
 
 
