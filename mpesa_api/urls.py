8685277bf2708a7ab06170d704d3a65fb96dade3
Alice-Githui <gnyokabi.alice@gmail.com>
2021-06-28 01:24:28 +0300

    add slashes to url

--- mpesa_api/urls.py
+++ mpesa_api/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns=[
     path('access/token/', views.getAccessToken, name="get_mpesa_access_token"),
-    path('online/lipa/', views.lipa_na_mpesa_online, name="lipa_na_mpesa"),
+    path('online/lipa/<str:category>/', views.lipa_na_mpesa_online, name="lipa_na_mpesa"),
 
     # register, confirmation, validation and callback urls
     path('c2b/register/', views.register_urls, name="register_mpesa_validation"),
