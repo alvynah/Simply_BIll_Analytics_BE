5dc19c0ca9b0d60942780b82b078673c58b06a06
Alice-Githui <gnyokabi.alice@gmail.com>
2021-06-28 00:48:21 +0300

    import JsonResponse

--- mpesa_api/views.py
+++ mpesa_api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 import requests
 from requests.auth import HTTPBasicAuth
 import json
