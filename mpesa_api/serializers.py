50b473971fb2a15329326e9a5f80bc01d443d46c
Alice-Githui <gnyokabi.alice@gmail.com>
2021-06-28 20:15:03 +0300

    remove mpesa app

--- mpesa_api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from mpesa_api.models import *
-
-class lipaNaMpesaOnline(serializers.ModelSerializer):
-    class Meta:
-        model=MpesaPayment
-        fields="id"
